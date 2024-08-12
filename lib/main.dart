1c1f5f4e64ccc0ce51b7e660c604654400bed484
Jboy422 <gitonly543@gmail.com>
2024-08-12 10:35:54 +0900

    doc: for

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,3 @@
-sealed class HttpState {}
-
-class Success extends HttpState {
-  final String data;
-
-  Success(this.data);
-}
-
-class Failure extends HttpState {
-  final String error;
-  final int code;
-
-  Failure(this.error, this.code);
-}
-
-HttpState handle(int code) {
-  switch (code) {
-    case 200:
-      return Success('Success');
-    default:
-      return Failure('Error', code);
-  }
+void main(L) {
+  
 }
